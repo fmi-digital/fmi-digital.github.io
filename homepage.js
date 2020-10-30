4961852019339cd081938c7e6da55e6bf389a180
kaushalsapre <15111468+kaushalsapre@users.noreply.github.com>
2020-10-31 01:09:26 +0530

    made some aesthetic changes

--- homepage.js
+++ homepage.js
@@ -16,7 +16,7 @@
 // function changeColor(){
 //   liveTable.style('background-color', 'red');
 // }
-// 
+//
 // let liveTable;
 // let columns;
 //
@@ -28,3 +28,34 @@
 // // columns.style('background-color', 'red');
 // liveTable.rows[0].cells.style('background-color', 'blue');
 // }
+let r1, g1, b1, r2, g2, b2;
+let div = 100;
+
+let canvas;
+function setup(){
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  noCursor();
+  r1 = 177;
+  g1 = 176;
+  b1 = 255;
+  r2 = 1;
+  g2 = 0;
+  b2 = 64;
+}
+
+function draw(){
+  background(255);
+
+  noStroke();
+  fill(120, 150, 100);
+  ellipse(mouseX, mouseY, 1000, 1000);
+  for (let i = 0; i < 100; i++){
+    // strokeWeight(1);
+    fill(r1 + ((r2 - r1)/div)*i, g1 + ((g2 - g1)/div)*i, b1 + ((b2 - b1)/div)*i);
+    noStroke();
+    rect(0,i + i*height/div, width, height/div);
+    }
+
+}
