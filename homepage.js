0b0237de94f1b4627998f4031801c6b1cf9f24fd
kaushalsapre <15111468+kaushalsapre@users.noreply.github.com>
2020-10-30 23:06:33 +0530

    made responsive. Wireframe more or less ready

---
+++ homepage.js
@@ -0,0 +1,30 @@
+// let liveTable;
+// let tablewWapper;
+//
+// function setup(){
+//   tableWrapper = select('.table-wrapper');
+//   liveTable = select('#liveTable');
+//   tableWrapper.position(500, 500);
+//
+//   console.log(liveTable.getColumnCount());
+// }
+//
+// function draw(){
+//     liveTable.mouseOver(changeColor);
+// }
+//
+// function changeColor(){
+//   liveTable.style('background-color', 'red');
+// }
+// 
+// let liveTable;
+// let columns;
+//
+// function setup(){
+// liveTable = document.getElementById("liveTable");
+// columns = document.getElementById("columns");
+// console.log(liveTable.rows[1].cells.length);
+// // columns.style();
+// // columns.style('background-color', 'red');
+// liveTable.rows[0].cells.style('background-color', 'blue');
+// }
